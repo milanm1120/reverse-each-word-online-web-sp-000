93fdc07c80d111ac76a0dbc04ce46fd9cfc53aa6
Milan Mody <milanm1120@gmail.com>
2021-02-15 22:52:03 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(string)
+  original = string.split(" ")
+  reversed = []
+  original.each do |word|
+    reversed<<word.reverse
+  end
+   reversed.join(" ")
+end
+
+def reverse_each_word(string)
+  original = string.split(" ")
+  reversed = []
+  original.collect do |word|
+    reversed<<word.reverse
+  end
+  reversed.join(" ")
+end
+
+print reverse_each_word("Hello there, and how are you?")
